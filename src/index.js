23070fc9748c22cbd13474ad54e37f5de87081c6
Afiur Rahman Fahim <afiur.fahim@gmail.com>
2018-03-03 16:59:22 +0600

    imported and initialized firebase

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as firebase from 'firebase';
+import firebaseConnfig from './utils/firebaseConfig';
 import './styles/index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
+
+firebase.initializeApp(firebaseConnfig);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
