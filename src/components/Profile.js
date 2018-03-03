3656e79299152fbf21257e7dcc4cc42a4dbe3cd2
Afiur Rahman Fahim <afiur.fahim@gmail.com>
2018-03-03 14:46:52 +0600

    modified element structure

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import avatar from '../assets/images/avatar.jpg';
+import '../styles/Profile.css';
 
 function Profile(props) {
   return(
@@ -8,8 +9,13 @@ function Profile(props) {
         <img src={avatar} alt="Avatar :P"/>
         <h2>Hi, User!</h2>
       </div>
-      <div className="user-action">
-        <button>Where have you been lately?</button>
+      <div className="user-posts">
+        <div className="user-action">
+          <button>Where have you been lately?</button>
+        </div>
+        <div className="user-feed">
+          <p className="empty-notice">Your posts will show up here...</p>
+        </div>
       </div>
     </div>
   )
