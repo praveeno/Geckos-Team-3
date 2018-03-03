4070e397fcf733ea3e7e77d35f47cfda2b2652bb
Afiur Rahman Fahim <afiur.fahim@gmail.com>
2018-03-03 13:01:53 +0600

    rendering profile component by default

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import Profile from '../components/Profile';
 import '../styles/App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header/>
+        <Profile/>
       </div>
     );
   }
